86aaf8581b4eb9939bd98d08af46834a836d10a2
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2024-02-22 15:01:18 +0100

    First commit

--- src/main/java/samuelesimeone/eserciziou5w3d4/User.java
+++ src/main/java/samuelesimeone/eserciziou5w3d4/User.java
@@ -1,2 +1,17 @@
-package samuelesimeone.eserciziou5w3d4;public class User {
+package samuelesimeone.eserciziou5w3d4;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class User {
+    private String name;
+    private String surname;
 }
+
