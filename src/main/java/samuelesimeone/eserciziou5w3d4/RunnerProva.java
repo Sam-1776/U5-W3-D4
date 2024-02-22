78459beda59ead5f7a657f7040a24e4d29224010
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2024-02-22 15:33:29 +0100

    add function firmaUser and test it

--- src/main/java/samuelesimeone/eserciziou5w3d4/RunnerProva.java
+++ src/main/java/samuelesimeone/eserciziou5w3d4/RunnerProva.java
@@ -1,2 +1,14 @@
-package samuelesimeone.eserciziou5w3d4;public class RunnerProva {
+package samuelesimeone.eserciziou5w3d4;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RunnerProva implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User("Andrea", "Sportillo");
+        System.out.println(user.firmaUser());
+    }
 }
